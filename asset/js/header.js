68c103ef62e805be2708c128377b9ecadd9a858b
erdemir123 <sadikerdemir93@gmail.com>
2022-09-21 13:31:55 +0300

    travel website sayfa düzeni oluşturuldu

---
+++ asset/js/header.js
@@ -0,0 +1,33 @@
+const header = document.querySelector("header")
+window.addEventListener("scroll",()=>{
+    
+    if(window.scrollY>100){
+        header.classList.add("active")
+    }
+    else{
+        header.classList.remove("active")
+    }
+})
+const search = document.querySelector("#search-btn")
+const search_div= document.querySelector(".search")
+const search_close= document.querySelector("#search-close")
+search.addEventListener("click",()=>{
+    search_div.classList.add("active")
+})
+search_close.addEventListener("click",()=>{
+    search_div.classList.remove("active")
+})
+
+const navbar = document.querySelector(".navbar-nav")
+const menu_btn = document.querySelector("#menu-btn")
+menu_btn.addEventListener("click",()=>{
+    navbar.classList.toggle("active")
+})
+
+
+const nav_close = document.querySelector("#nav-close")
+nav_close.addEventListener("click",()=>{
+    navbar.classList.remove("active")
+})
+
+
