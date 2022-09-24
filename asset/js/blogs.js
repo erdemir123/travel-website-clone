719ad13470fd738d1d35e6aace210e885ef71081
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-09-24 18:05:29 +0300

    services and blogs sections finished

---
+++ asset/js/blogs.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 10,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
